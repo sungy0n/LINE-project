8cc6a214183b53620764ead7f30de6a912f10425
kimseongyeon <seongyeon0810@naver.com>
2025-10-31 18:40:41 +0900

    Footer 퍼블리싱

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,24 +9,30 @@ const Footer = () => {
     <div id="Footer_wrap">
         <div className="content_top">
             <img className='line' src={line_icon} alt="" />
-            <div className="app_store">
+            <div id='line' className="app_store">
                 <img src={apple} alt="" />
                 App Store
             </div>
-            <div className="google_play">
+            <div id='line' className="google_play">
                 <img src={google} alt="" />
                 Google Play
             </div>
-            <div className="desktop">
+            <div id='line' className="desktop">
                 {/* <img src="" alt="" /> */}
                 Desktop
             </div>
-            <div className="windows_store">
+            <div id='line' className="windows_store">
                 {/* <img src="" alt="" /> */}
                 Windows Store
             </div>
-            <div className="chrome">
+            <div id='line' className="chrome">
                 Chrome
+            </div>
+            <div id='line' className="apk">
+                APK
+            </div>
+            <div className="screen_saver">
+                Screen Saver
             </div>
         </div>
         <div className="content_bottom">
@@ -34,13 +40,13 @@ const Footer = () => {
                 <div className="text1">
                 ©️ LY Coperation
                 </div>
-                <div className="text2">
+                <div id='line' className="text2">
                     도움말
                 </div>
-                <div className="text3">
+                <div id='line' className="text3">
                     이용약관
                 </div>
-                <div className="text4">
+                <div id='line' className="text4">
                     개인정보 처리방침
                 </div>
                 <div className="text5">
